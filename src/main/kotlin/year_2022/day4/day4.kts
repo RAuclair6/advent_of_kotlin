f271f5748d9b459e754c93b306a2331aef2f84db
Ronald Auclair <buddy.auclair6@gmail.com>
2022-12-04 20:57:18 -0800

    2022 day 4 refactor

--- src/main/kotlin/year_2022/day4/day4.kts
+++ src/main/kotlin/year_2022/day4/day4.kts
@@ -1,42 +1,25 @@
 package year_2022.day4
 
-import org.w3c.dom.ranges.Range
 import java.io.File
 
-fun pt1(input: List<List<Pair<Int, Int>>>): Int {
-    var ans = 0
-    for (line in input) {
-        if ((line[0].first - line[1].first <= 0) && (line[0].second - line[1].second >= 0) ||
-            (line[1].first - line[0].first <= 0) && (line[1].second - line[0].second >= 0)
-        ) {
-            ans += 1
-        }
-    }
-    return ans
-}
-
-fun pt2(input: List<List<Pair<Int, Int>>>): Int {
-    var ans = 0
-    for (line in input) {
-        if (line[0].first <= line[1].second && line[0].second >= line[1].first) {
-            ans += 1
-        }
-    }
-    return ans
-}
-
 fun main(args: Array<String>) {
-    val inp: List<List<Pair<Int, Int>>> = File(args[0])
+    val inp: List<List<IntRange>> = File(args[0])
         .readLines()
         .map {
             it.split(",").map { inner ->
                 val spl = inner.split("-")
-                Pair(spl[0].toInt(), spl[1].toInt())
+                spl[0].toInt()..spl[1].toInt()
             }
         }
-//    println("$inp")
-    println("pt1: ${pt1(inp)}")
-    println("pt2: ${pt2(inp)}")
+    println(
+        "pt1: ${
+            inp.count {
+                (it[0].first - it[1].first <= 0) && (it[0].last - it[1].last >= 0) || 
+                (it[1].first - it[0].first <= 0) && (it[1].last - it[0].last >= 0)
+            }
+        }"
+    )
+    println("pt2: ${inp.count { it[0].first <= it[1].last && it[0].last >= it[1].first }}")
 }
 
 main(args)
