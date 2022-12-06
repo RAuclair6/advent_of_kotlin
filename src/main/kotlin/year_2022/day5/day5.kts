91a9729191b59c839a99d586cd935ad01018db36
Ronald Auclair <buddy.auclair6@gmail.com>
2022-12-05 22:17:15 -0800

    2022 day 5

--- src/main/kotlin/year_2022/day5/day5.kts
+++ src/main/kotlin/year_2022/day5/day5.kts
@@ -1,5 +1,26 @@
 import java.io.File
-import java.util.Deque
+
+fun pt1(input: MutableMap<Int, ArrayDeque<Char>>, insts: List<List<Int>>) {
+    for (line in insts) {
+        for (n in 0 until line[0]) {
+            val f = input[line[1] - 1]?.removeFirstOrNull() ?: break
+            input[line[2] - 1]?.addFirst(f)
+        }
+    }
+    println(input)
+}
+
+fun pt2(input: MutableMap<Int, ArrayDeque<Char>>, insts: List<List<Int>>) {
+    for (line in insts) {
+        var l = mutableListOf<Char>()
+        for (n in 0 until line[0]) {
+            val c = input[line[1] - 1]?.removeFirstOrNull() ?: break
+            l.add(c)
+        }
+        input[line[2] - 1]?.addAll(0, l)
+    }
+    println(input)
+}
 
 fun main(args: Array<String>) {
     val inp: List<String> = File(args[0]).readText().split("\n\n")
@@ -14,7 +35,7 @@ fun main(args: Array<String>) {
             .replace("][", "")
             .padEnd(9, 'x')
         }
-    var craneMap: MutableMap<Int, ArrayDeque<Char>> = mutableMapOf()
+    var craneMap: MutableMap<Int, ArrayDeque<Char>> = mutableMapOf<Int, ArrayDeque<Char>>()
     for (i in 0..8) {
         craneMap[i] = ArrayDeque()
         for (y in cranes.indices) {
@@ -22,6 +43,16 @@ fun main(args: Array<String>) {
         }
     }
     val insts = inp[1]
+        .split("\n")
+        .map {
+            it
+                .replace("move ", "")
+                .split(" from | to ".toRegex()).map { num ->
+                    num.toInt()
+                }
+        }
+//    println("pt1: ${pt1(craneMap, insts)}")
+    println("pt2: ${pt2(craneMap, insts)}")
 }
 
 main(args)
